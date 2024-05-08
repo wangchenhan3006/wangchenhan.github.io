097b91f2b74bb1d2ac433bfc3330449f0988ed63
wch3306 <zklco06685@126.com>
2024-05-08 21:22:01 +0800

    Site updated: 2024-05-08 21:21:55

---
+++ anzhiyu/random.js
@@ -0,0 +1,1 @@
+var posts=["2024/02/23/Linked/","2024/02/23/List/","2024/03/26/Unity笔记-json和xml/","2023/12/10/Unity笔记-声音管理器/","2023/12/08/Unity笔记-UI框架/","2023/12/04/Unity笔记-泛型单例/","2023/12/04/Unity笔记-消息框架/","2024/02/23/tips/","2023/12/06/Unity笔记-状态机/","2024/03/16/npm自建图床/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
